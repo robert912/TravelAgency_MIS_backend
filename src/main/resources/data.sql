8e1c95edac32fe0ca3ea3ee3e36e204dfdcfe85a
Roberto <roberto.orellana.t@usach.cl>
2026-05-12 19:18:49 -0400

    data base update user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -197,4 +197,4 @@ INSERT IGNORE INTO `tour_package_service` (`id`, `tour_package_id`, `service_id`
 
 INSERT IGNORE INTO persons (id, full_name, identification, email, phone, nationality, failed_attempts, active, created_by_user_id, updated_by_user_id, created_at, updated_at) VALUES
 (1, 'Roberto Orellana Tamayo', '17411947-3', 'roberto.orellana.t@usach.cl', '974331446', 'Chilena', 3, 1, 1, 1, '2026-04-18 05:28:44.096003', '2026-04-18 05:28:44.096003'),
-(2, 'María Fernanda López González', '20567890-5', 'maria.lopez@travelapp.com', '987654321', 'Chilena', 0, 1, 1, 1,'2026-04-18 10:15:00.000000','2026-04-18 10:15:00.000000');
+(2, 'María Fernanda López González', '12345678-9', 'maria.lopez@usach.com', '987654321', 'Chilena', 3, 1, 1, 1,'2026-04-18 10:15:00.000000','2026-04-18 10:15:00.000000');
